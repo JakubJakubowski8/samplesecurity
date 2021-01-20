028483d382cad1f9efc7fbbbacf952008d4903b4
jakubjakubowski8 <jakubjakubowski8@gmail.com>
2021-01-20 02:34:24 +0100

    Initiate project

---
+++ src/main/java/com/jakub/samplesecurity/SampleSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.jakub.samplesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SampleSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSecurityApplication.class, args);
+	}
+
+}
